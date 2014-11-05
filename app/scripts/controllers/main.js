3f6a5447345c2f04c80979cba59baff1ae895eaf
Alex Mejias <alexdmejias@gmail.com>
2014-11-04 22:16:20 -0500

    working state

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,7 +10,6 @@
  function newSong(songName) {
   var song = new Audio(songName);
   song.loop = true;
-  song.play();
 
   return song;
  }
@@ -18,83 +17,94 @@
 angular.module('soundsApp')
   .constant('soundsBase', '/sounds/')
   .service('newSong', [newSong])
-  .controller('MainCtrl', function (soundsBase, Playing) {
+  .controller('MainCtrl', function ($filter, soundsBase) {
   var self = this;
 
-  // songs currently being played
-  self.playing = {};
   // boolean, whether there the sound is currently on
   self.globalSound = false;
+
+  self.playing = [];
 
   self.available = {
     rain: {
       title: 'Rain',
       url: 'rain.mp3',
-      status: false,
+      used: false,
+      playing: false
     },
     birds: {
       title: 'Birds',
       url: 'test.mp3',
-      status: false,
+      used: false,
+      playing: false
     }
   };
 
   self.toggleGlobalSound = function() {
-    if (self.globalSound === false) {
+    // get all the songs that are currently playing
+    self.playing = $filter('filter')(self.available, function(a) {
+      a.playing
+    }, true);
+    console.log($filter('filter')(self.available, function(song) {
+      return song.playing === true;
+    }, true));
+    console.info('wasd')
 
-      self.playing['rain'].play();
+    self.paused = [];
+    angular.forEach(self.playing, function(value, key) {
+    });
+
+    if (self.globalSound === false) {
+      for (sound in self.available) {
+        if (self.available[sound].playing) {
+          self.songPause(sound);
+        }
+      }
     } else {
-      self.playing['rain'].pause();
+      for (sound in self.available) {
+        if (self.available[sound].playing) {
+          self.songPlay(sound);
+        }
+      }
     }
     self.globalSound = !self.globalSound;
   }
 
-  // cycle through the available songs, and find
-  // return the index of the song that matches the 
-  // object name with the given param.
-  self._findbyName = function(name) {
-    for (var i = 0; i < self.available.length; i++) {
-      if (self.available[i].name == name) {
-        break;
-      }
-    }
-    return i;
-  }
-
-  // toggles the status of a song. Creates it if necessary
+  // toggles the used of a song. Creates it if necessary
   self.toggleSong = function(name) {
     if (typeof(self.available[name]['song']) == 'undefined') {
-        self.songAdd(name);
+      self.songAdd(name);
     } else {
-        if (self.available[name].status) {
-            self.songRemove(name);
-        } else {
-            self.available[name].song.play();
-            self.available[name].status = !self.available[name].status;
-        }
+      if (self.available[name].playing) {
+        self.songPause(name);
+      } else {
+        self.songPlay(name);
+      }
     }
   }
 
   // adds a song to the self.playing model
   self.songAdd = function(name) {
     self.available[name].song = newSong(soundsBase + self.available[name].url);
-    self.available[name].status = !self.available[name].status;
+    self.available[name].used = true;
+    self.songPlay(name);
   }
+
+  self.songPlay = function(name) {
+    self.available[name].playing = true;
+    self.available[name].song.play();
+  }
+
   // removes a song from the self.playing model
-  self.songRemove = function(name) {
+  self.songPause = function(name) {
+    self.available[name].playing = false;
     self.available[name].song.pause();
-    self.available[name].status = !self.available[name].status;
+    // self.available[name].used = !self.available[name].used;
   }
 
   // toggles play/pause of the application
   self.playToggle = function() {
     self.toggleGlobalSound();
   }
-
-  // event callback to when a song is clicked
-  // title: the title property of the song clicked
-  self.onClick = function(title) {
-    self.toggleSong(title);
-  };
 
 });
